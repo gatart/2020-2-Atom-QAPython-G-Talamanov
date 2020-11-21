9559eb86585acf2703c5d779f9dfad6cef0280ef
gatart <talamanov01@mail.ru>
2020-11-21 12:32:04 -0600

    Made similar args

--- QA-Python-homework-4/analize.sh
+++ QA-Python-homework-4/analize.sh
@@ -4,16 +4,16 @@ outfile=analyzed
 
 print_help(){
     cat <<EOF
-usage: analyze.sh  -a | -m | -b | -c | -s  <FILE> | <DIR>
--a
+usage: analyze.sh  a | m | b | c | s  <FILE> | <DIR>
+a
     кол-во всех запросы
--m
+m
     кол-во запросов по методам
--b
+b
     топ самых больших запросов
--c
+c
     топ запросов с клиентской ошибкой
--s
+s
     топ с серверной ошибкой
 FILE
     входной файл
@@ -65,19 +65,19 @@ case "$1" in
         rm $outfile
         print_help >&2
         ;;
-    -a)
+    a)
         egrep -w 'OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT' $file | wc -l
         ;;
-    -m)
+    m)
         egrep -w 'OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT' $file | awk -F '[ "]' '{print $7}' | sort | grep -E $methods | uniq -c | sort -rg | awk '{print $2,$1}'
         ;;
-    -b)
+    b)
         egrep -w 'OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT' $file | awk -F '[ "]' '{print $12,$11,$8}' | sort -k 3,3 -k 1,1rn | uniq -c -f 2 | sort -k 2,2rn | head -10 | awk '{print $4,$3,$1}'
         ;;
-    -c)
+    c)
         egrep -w 'OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT' $file | awk -F '[ "]' '$11>=400 && $11<500 {print $11,$1,$8}' | sort -k 3,3 | uniq -c -f 2 | sort -k 1,1rn | head -10 | awk '{print $4,$2,$3}'
         ;;
-    -s)
+    s)
         egrep -w 'OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT' $file | awk -F '[ "]' '$11>=500 && $11<600 {print $12,$8,$11,$1}' | sort -rnk 1,1 | head -10 | awk '{print $4, $3, $2}'
         ;;
     *)
